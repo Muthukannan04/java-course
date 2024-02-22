f369b3e8030d4e41e7dcd2fa785ed0c1b6bcdf2e
Muthukannan04 <muthukannan883@gmail.com>
2024-02-22 09:40:37 +0530

    Updated

--- LinkedList.java
+++ LinkedList.java
@@ -1,36 +1,35 @@
-import java.util.*;
-
 class Node{
-	int data;
-	Node next;
-	Node(int data){
-	this.data=data;
-}
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
 }
 class LinkedList{
-	Node head;
-	LinkedList(){
-	  this.head=null;
-}
-void addfirst(int data){
-Node newnode=new Node(data);
-newnode.next=head;
-head=newnode;
-}
-void print(){
-	Node temp=head;
-	while(temp!=null){
-		System.out.println(temp.data);
-	temp=temp.next;
-}
-}
+    Node head;
+    LinkedList(){
+        this.head=null;
+    }
+    void addfirst(int data){
+        Node newnode=new Node(data);
+        newnode.next=head;
+        head=newnode;
+    }
+    void print(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
 }
 public class Main{
-	public static void main(String[] args){
-	LinkedList l1=new LinkedList();
-	l1.addfirst(30);
-	l1.addfirst(40);
-	l1.addfirst(60);
-	l1.print();
-}
+    public static void main(String[] args){
+        LinkedList l1=new LinkedList();
+        l1.addfirst(10);
+        l1.addfirst(20);
+        l1.addfirst(30);
+        l1.print();
+    }
 }
